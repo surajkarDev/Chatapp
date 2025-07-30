c672a14736ec6d0738b641a46a23b2d8eadcc53b
suraj kar <suraj.kar@farenexus.com>
2025-07-30 22:34:42 +0530

    issue fixed

--- app/components/Childcomp/page.tsx
+++ app/components/Childcomp/page.tsx
@@ -1,25 +1,28 @@
-import React,{useImperativeHandle, forwardRef,useRef,memo} from "react";
+"use client";
+import React, { useImperativeHandle, forwardRef, useRef, memo } from "react";
 
-export type ChildCompProps = {
-    focus: () => void;
-}
+export type ChildCompRef = {
+  focus: () => void;
+  handlerClick: () => void;
+};
 
-const ChildComp = forwardRef<ChildCompProps, {}>((props, ref) => {
-    const inputRef = useRef<HTMLInputElement>(null);
-    const handlerClick = () => {
-        if (inputRef.current) {
-            inputRef.current.value = "Hello World!";
-        }
-    }     
-    useImperativeHandle(ref, () => ({
-        focus: () => {
-            inputRef.current?.focus();
-        },
-        handlerClick:handlerClick
-    }));
-         
+const ChildComp = forwardRef<ChildCompRef, {}>((props, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    return <input ref={inputRef} />;
+  const handlerClick = () => {
+    if (inputRef.current) {
+      inputRef.current.value = "Hello World!";
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputRef.current?.focus();
+    },
+    handlerClick,
+  }));
+
+  return <input ref={inputRef} className="border px-2 py-1 rounded" />;
 });
 
 export default memo(ChildComp);
